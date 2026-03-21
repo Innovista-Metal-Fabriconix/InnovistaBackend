ac475e8363a054b4c1743cca84cbbd728f280c6d
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2026-03-21 14:47:53 +0530

    Add the localy run fix the type errors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
+let cachedServer: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', process.env.FRONTEND_URL || ''],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
@@ -22,6 +24,21 @@ async function bootstrap() {
   );
   app.use(cookieParser());
 
-  await app.listen(4000);
+  if (process.env.VERCEL) {
+    await app.init();
+    return app.getHttpAdapter().getInstance();
+  }
+
+  await app.listen(process.env.PORT ?? 4000);
 }
-bootstrap();
+
+if (!process.env.VERCEL) {
+  bootstrap();
+}
+
+export default async function handler(req: any, res: any) {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  return cachedServer(req, res);
+}
